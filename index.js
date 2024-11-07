a2f9d909e6427e98d2a0c7f70223e2f401d1990e
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-07 08:08:41 +0100

    packages installed and connection

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+import express from "express";
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+
+//configure env file
+dotenv.config();
+
+//rest object
+const app = express();
+
+//rest api
+app.get('/', (req, res) => {
+    res.send(
+        '<h1>Welcome to e-commerce-mern</h1>'
+    )
+})
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`.bgCyan.white)
+})
