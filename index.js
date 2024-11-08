7f96903083ad4a4e3a339effa24ac4dc19b1b1c9
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-08 04:59:44 +0100

    user model and routes create

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from "./config/db.js";
 import morgan from 'morgan';
+import authRoutes from './routes/authRoutes.js';
 
 //configure env file
 dotenv.config();
@@ -17,6 +18,10 @@ const app = express();
 //middleware
 app.use(express.json())
 app.use(morgan('dev'))
+
+
+//routes
+app.use('api/v1/auth', authRoutes)
 
 
 //rest api
