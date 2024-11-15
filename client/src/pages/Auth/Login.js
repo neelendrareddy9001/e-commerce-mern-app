d92f433f5ba351f6ca157b853f812ceb1139dd68
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-15 05:35:08 +0100

    create d auth context provided to chhildren

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 
+import { useAuth } from "../../context/Auth";
+
 const Login = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <Layout>
       <h1>Login Page</h1>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/Auth";
 
 const HomePage = () => {
-  return <Layout title={"Best Offers"}>HomePage</Layout>;
+  const [auth, setAuth] = useAuth();
+  return (
+    <Layout title={"Best Offers"}>
+      <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    </Layout>
+  );
 };
 
 export default HomePage;
