f71e9fa992e91b627ff57dd24bc00bb522874ab2
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-15 07:41:14 +0100

    forgot component with login styles navigate to it

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 
 import { useAuth } from "../../context/Auth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
   const [auth, setAuth] = useAuth();
+
+  const location = useLocation();
+
+  const handleSubmit = () => {};
+
   return (
-    <Layout>
-      <h1>Login Page</h1>
+    <Layout title="Register - Ecommerce App">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">LOGIN FORM</h4>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <Layout title={"Best Offers"}>
       <h1>HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+      {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
     </Layout>
   );
 };
