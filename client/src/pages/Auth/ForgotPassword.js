5283e132835a8397965c200916d69a3555c08a89
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-15 09:33:21 +0100

    forgot password component with styles

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+
+import axios from "axios";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const ForgotPassword = () => {
-  return <div></div>;
+  const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {};
+  return (
+    <Layout title="Forgot Password - Ecommerce App">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">RESET PASSWORD</h4>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="enter your email"
+              className="form-control"
+              id="exampleInputEmail"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              id="exampleInputAnswer"
+              placeholder="Enter you favorite sport name"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            RESET
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default ForgotPassword;
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
-
+import "../../styles/AuthStyles.css";
 import { useAuth } from "../../context/Auth";
 import { useLocation, useNavigate } from "react-router-dom";
 
