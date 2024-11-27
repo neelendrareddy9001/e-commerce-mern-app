dd9d543fcf09fa420efeda8b8fdb462ec9afaa2c
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-27 23:54:55 +0100

    category routes  created

---
+++ controllers/cateGoryController.js
@@ -0,0 +1,8 @@
+import categoryModel from "../models/categoryModel";
+
+export const cateGoryController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("error", error);
+  }
+};
--- routes/cateGoryRoutes.js
+++ routes/cateGoryRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+import { cateGoryController } from "../controllers/cateGoryController";
+
 const router = express.Router();
+
+router.post("/create-category");
 
 export default router;
